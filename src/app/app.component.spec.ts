44082cbd0dbc2f1b3edefdf520c1f83ebf4d5d8c
Fabricio Toyota, Sinqia (800-Extern) <fabricio.toyota@sinqia.com.br>
2022-10-19 16:39:21 -0300

    second commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,5 +15,4 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
- 
 });
--- src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared';
 
 describe('JogoDaVelhaComponent', () => {
   let component: JogoDaVelhaComponent;
   let fixture: ComponentFixture<JogoDaVelhaComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ JogoDaVelhaComponent ]
+    await 
+      TestBed.configureTestingModule({
+      declarations: [ JogoDaVelhaComponent ],
+      providers: [ JogoDaVelhaService ]
     })
     .compileComponents();
 
@@ -17,7 +20,4 @@ describe('JogoDaVelhaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
